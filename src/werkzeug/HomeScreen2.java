d6ffcc6d17b2e2631fc3604a700bb2c16ee652f7
QuocHuyNguyen <quochuyjava@users.noreply.github.com>
2019-09-25 23:05:43 +0200

    refactoring UI

---
+++ src/werkzeug/HomeScreen2.java
@@ -0,0 +1,54 @@
+package werkzeug;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+
+public class HomeScreen2 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomeScreen2 frame = new HomeScreen2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HomeScreen2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblaktuellekanal = new JLabel("_aktuelleKanal");
+		contentPane.add(lblaktuellekanal, BorderLayout.NORTH);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.EAST);
+		
+		JList list = new JList();
+		scrollPane.setColumnHeaderView(list);
+	}
+
+}
--- src/werkzeug/HomeScreenWerkzeug.java
+++ src/werkzeug/HomeScreenWerkzeug.java
@@ -1,10 +1,13 @@
 package werkzeug;
 
+import java.io.File;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import Service.HomeScreenService;
@@ -22,6 +25,7 @@ public class HomeScreenWerkzeug
         _homeSreenService = homeSreenService;
         showUI();
         showChanelListe();
+//        zeigeAktuelleInhalt();
     }
 
     /**
@@ -52,4 +56,19 @@ public class HomeScreenWerkzeug
         }
         
     }
+    
+//  private void zeigeAktuelleInhalt()
+//  {
+//  	JPanel aktuelleFrame = _homeScreenUI.get_aktuelleInhalt();
+//  	
+//  	File file = new File("Users/quochuy/Downloads/maxresdefault.jpg");
+//  	ImageIcon imageIcon = new ImageIcon(file.getName());
+//  	JLabel label = new JLabel(imageIcon);
+//  	
+//  	aktuelleFrame.add(label);
+//  	System.out.println("da show hinh");
+//  	
+//  	_homeScreenUI.set_aktuelleInhalt(aktuelleFrame);
+//  	
+//  }
 }
